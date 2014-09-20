7fc326a0332ab57ae01f0687efcc3d4de852bba1
Catherine O'Neill <cathzozo@gmail.com>
2014-09-20 07:53:53 -0500

    trying to get devise and omniauth to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
       data = access_token.info
