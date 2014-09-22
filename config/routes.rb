c806b1d762c199ad5286438d2be885161c78c12a
Catherine O'Neill <cathzozo@gmail.com>
2014-09-22 14:48:39 -0500

    eliminated #show and #new paths

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout  
 
-  resources :users
+  resources :users, except: [:index, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
