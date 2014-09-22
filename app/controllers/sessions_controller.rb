508644e928b09a820fec0705ce98040b6e4112b0
Catherine O'Neill <cathzozo@gmail.com>
2014-09-22 16:20:20 -0500

    changed redirect for new user vs. existing user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,19 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) 
+    if user.nil?
+      user = User.create_with_omniauth(auth)
+      new_user = true
+    end
     user.profile_pic = auth["info"]["image"]
-    user.save
     session[:user_id] = user.id
-    redirect_to edit_user_path(user), :alert => "Signed in!"
+    user.save
+    if new_user
+      redirect_to edit_user_path(user)
+    else
+      redirect_to user_path(user)
+    end
   end
  
   def destroy
