ee9f230cdd65c30032d4cafa85efc146eae9fb53
Catherine O'Neill <cathzozo@gmail.com>
2014-09-26 09:07:20 -0500

    removed puts statement

--- app/transaction_scripts/find_closest_station.rb
+++ app/transaction_scripts/find_closest_station.rb
@@ -1,6 +1,5 @@
 class FindClosestStation
   def self.run(latitude, longitude, user_id)
-    puts "IN NEAREST STATION!"
     nearby_stations = WeatherStation.near("#{latitude}, #{longitude}", 5, order: "distance")
     user = User.find(user_id)
     if closest = nearby_stations.first
