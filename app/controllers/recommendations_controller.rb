fd569642153ddabce170343b99b1e0957d814123
Catherine O'Neill <cathzozo@gmail.com>
2014-09-27 20:40:45 -0500

    recommendations in place

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -5,13 +5,18 @@ class RecommendationsController < ApplicationController
   def index
     @user = current_user
     @yard = current_user.yard
-    @recommendations = @yard.recommendations
+    @recommendations = @yard.recommendations.order(created_at: :desc)
   end
 
   # GET /recommendations/1
   # GET /recommendations/1.json
   def show
+    @user = current_user
+    @yard = current_user.yard
     @recommendation = Recommendation.find(params[:id])
+    @watering_date = @recommendation.created_at.to_date + 1 
+    @reports = WeatherData.where(date: @watering_date-8..@watering_date-2, 
+      weather_station_id: @user.weather_station_id).order(date: :desc)
   end
 
 end
