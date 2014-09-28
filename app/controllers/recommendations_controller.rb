59701c85b27c367f6d7e792c4d398f9a2405752c
Catherine O'Neill <cathzozo@gmail.com>
2014-09-28 15:18:38 -0500

    corrected for no current recommendation yet

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -7,9 +7,13 @@ class RecommendationsController < ApplicationController
     @user = current_user
     @yard = current_user.yard
     @recommendation = @yard.recommendations.order(created_at: :desc).first
-    @weather_reports = @recommendation.weather_datas.order(date: :desc)
-    @eto_reports = @recommendation.eto_calculations.order(date: :desc)
-    render 'show'
+    if @recommendation
+      @weather_reports = @recommendation.weather_datas.order(date: :desc)
+      @eto_reports = @recommendation.eto_calculations.order(date: :desc)
+      render 'show'
+    else
+      redirect_to profile_path, notice: "You do not have a recommendation yet.  Make sure you have entered all your information, and we will send you one soon!"
+    end
   end
 
 end
