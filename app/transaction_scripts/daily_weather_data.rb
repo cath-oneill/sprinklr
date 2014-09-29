f8ec937ea281b3dd7fd921246c56c73e74050679
Catherine O'Neill <cathzozo@gmail.com>
2014-09-29 10:32:01 -0500

    set time to spread out API hits by minute

--- app/transaction_scripts/daily_weather_data.rb
+++ app/transaction_scripts/daily_weather_data.rb
@@ -4,9 +4,10 @@ require 'json'
 class DailyWeatherData
   def self.run(query = "yesterday") #alternately put in history_YYYYMMDD
     weather_stations = WeatherStation.where(bad_data: false)
-
+    timer = 1
     weather_stations.each do |sta|
-      DailyWeatherWorker.perform_async(sta.id, query)
+      timer += 1
+      DailyWeatherWorker.perform_in(timer.minutes, sta.id, query)
     end
 
   end
