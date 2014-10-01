8dd417415fc443deefcba8ce3f05c3624499c8b1
Catherine O'Neill <cathzozo@gmail.com>
2014-10-01 17:24:02 -0500

    form works

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,11 +4,26 @@
 var width = 1000,
     height = 1000,
     center = [-97.72409, 30.279262],
+    YYYY,
+    MM,
+    DD,
+    mapType = "eto",
     projection,
     path,
     svg,
     zips,
     url;
+
+$('#map-form').on('submit', function(e){
+    e.preventDefault();
+    date = $('#map-date').val().split("-");
+    YYYY = date[0];
+    MM = date[1];
+    DD = date[2];
+    mapType = $('#map-date').val();
+    coloredMap();
+    header();
+});
 
 $('#map').attr("width", width).attr("height", height);
 
@@ -26,39 +41,60 @@ svg = d3.select("#map").append("svg")
         height: height
     });
 
-
 zips = svg.append("g")
     .attr({
         class: "zips",
         transform: "translate(-300, 450) rotate(-53)"
     });
+
+function coloredMap() {
  
-url = "./eto/2014/09/30.json";
+    url = "./eto/" + YYYY + "/" + MM + "/" + DD + ".json";
 
-d3.json( url, function( res ) {
-    var paired_data = {};
-    $.each( res, function(i, report ) {
-        key = report["weather_station"]["zip"];
-        value = report["eto"];
-        paired_data[key] = value;
-    });
-
-    d3.json("austinzips.geojson", function(err, data) {
-        console.log(data);
-        $.each(data.features, function(i, feature) {
-        zips.append("path")
-            .datum(feature.geometry)
-            .attr("id", feature.properties.NAME)
-            .attr("d", path)
-            .attr("class", quantize(feature.properties.NAME));
+    d3.json( url, function( res ) {
+        var paired_data = {};
+        $.each( res, function(i, report ) {
+            key = report["weather_station"]["zip"];
+            value = report["eto"];
+            paired_data[key] = value;
         });
 
-        function quantize(zip) {
-            eto = paired_data[zip];
-            if (eto === undefined) { return }
-            return "q" + Math.min(8, ~~(eto * 36)) + "-9";
+        d3.json("austinzips.geojson", function(err, data) {
+            $.each(data.features, function(i, feature) {
+            zips.append("path")
+                .datum(feature.geometry)
+                .attr("id", feature.properties.NAME)
+                .attr("d", path)
+                .attr("class", quantize(feature.properties.NAME));
+            });
+
+            function quantize(zip) {
+                eto = paired_data[zip];
+                if (eto === undefined) { return }
+                return "q" + Math.min(8, ~~(eto * 36)) + "-9";
+            }
+        });
+    });
+}
+
+function getYesterdaysDate() {
+    var date = new Date();
+    date.setDate(date.getDate()-1);
+    DD = date.getDate(); 
+    MM = date.getMonth()+1;
+    YYYY = date.getFullYear();
+}
+
+
+function header() {
+    $('#map-title').text(function(){
+        if (mapType === 'eto') {
+            return "Calculated Evapotranspiration";
         }
     });
-});
+    $('#date-title').text(MM + "-" + DD + "-" + YYYY);
+}
 
-
+getYesterdaysDate();
+coloredMap();
+header();
--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,6 +2,8 @@ class MapController < ApplicationController
 before_filter :disable_nav
 
   def map
+    @date = Date.today - 1
     render 'map'
   end
+
 end
